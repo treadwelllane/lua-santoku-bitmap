93e2e93285c00e7000c1d4959659364b31a0135d
Matthew Brooks <mb964033@gmail.com>
2025-04-21 16:11:19 +0000

    license

--- make.lua
+++ make.lua
@@ -1,8 +1,9 @@
 local env = {
 
   name = "santoku-bitmap",
-  version = "0.0.54-1",
+  version = "0.0.55-1",
   variable_prefix = "TK_BITMAP",
+  license = "MIT",
   public = true,
 
   cflags = { "-march=native", "-O3", "-Wall", "-Wextra", "-Wno-sign-conversion", "-Wsign-compare", "-Wstrict-overflow", "-Wpointer-sign", },
